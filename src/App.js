5c2e944897592499a026fe02c88287236a54f086
davified <davified@gmail.com>
2018-05-31 12:51:59 +0800

    Add react bootstrap

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>Sentiment Classifier</h1>
         <InputForm handleSubmit={this.handleSubmit.bind(this)} />
         {this.state.sentiment === "positive" && <PositiveSentiment />}
         {this.state.sentiment === "negative" && <NegativeSentiment />}
--- src/InputForm.js
+++ src/InputForm.js
@@ -1,20 +1,41 @@
 import React, { Component } from "react";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 
 class InputForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    };
+  }
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit.bind(this)}>
-          <input type="text" ref={input => (this.input = input)} />
-          <input type="submit" value="submit" />
-        </form>
+        <Form onSubmit={this.handleSubmit.bind(this)}>
+          <FormGroup>
+            <Label for="exampleEmail">Email</Label>
+            <Input
+              type="textarea"
+              placeholder="type something (e.g. this is great!, or this thing sucks!!"
+              value={this.state.value}
+              onChange={this.handleChange.bind(this)}
+            />
+          </FormGroup>
+          <Button>Submit</Button>
+        </Form>
       </div>
     );
   }
 
   handleSubmit(event) {
+    console.log("handleSubmit()");
+    console.log(this.input.value);
     event.preventDefault();
     this.props.handleSubmit(this.input.value);
+  }
+
+  handleChange(event) {
+    this.setState({ input: event.target.value });
   }
 }
 

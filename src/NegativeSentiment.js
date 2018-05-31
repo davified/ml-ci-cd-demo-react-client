1c9de79d8c32d4f5c6dbc5cc186c69a002bf259b
davified <davified@gmail.com>
2018-05-31 14:21:24 +0800

    Switch emojis to icons

--- src/NegativeSentiment.js
+++ src/NegativeSentiment.js
@@ -1,9 +1,10 @@
 import React from "react";
+import logo from "./icons/001-thumb-down.svg";
 
 const NegativeSentiment = props => (
   <div className="sentiment-box" id="negative">
     <span role="img" aria-label="negative sentiment">
-      negative 👎
+      negative <img id="sentiment-icon" src={logo} alt="negative sentiment" />
     </span>
   </div>
 );
--- src/PositiveSentiment.js
+++ src/PositiveSentiment.js
@@ -1,9 +1,10 @@
 import React from "react";
+import logo from "./icons/003-like.svg";
 
 const PositiveSentiment = props => (
   <div className="sentiment-box" id="positive">
     <span role="img" aria-label="positive sentiment">
-      positive 👍
+      positive <img id="sentiment-icon" src={logo} alt="positive sentiment" />
     </span>
   </div>
 );
